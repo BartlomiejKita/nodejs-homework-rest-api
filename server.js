0bcf3a85f09e93bc2e52ca310246a902d73fc89f
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-11-09 14:40:00 +0100

    add patch avatar

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 require("dotenv").config();
-
 const indexRouter = require("./routes/api/index");
-
 const app = express();
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./config/passport");
 
